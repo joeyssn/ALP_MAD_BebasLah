bd2f0515340afe165fe8f2b5753f2c46bd6358c9
Jevon <winartojevon@gmail.com>
2025-05-22 08:48:34 +0700

    Initial Commit

---
+++ ALP_MAD_Joey/ALP_MAD_JoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MAD_JoeyApp.swift
+//  ALP_MAD_Joey
+//
+//  Created by student on 22/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MAD_JoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
