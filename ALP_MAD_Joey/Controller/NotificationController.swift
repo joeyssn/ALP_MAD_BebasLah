a737cd04ad4c580a02b6951e148da942c866455f
Kevin Christian <git@kevinchr.com>
2025-06-05 02:53:59 +0700

    update controller notification user, edit profile

---
+++ ALP_MAD_Joey/Controller/NotificationController.swift
@@ -0,0 +1,8 @@
+//
+//  NotificationController.swift
+//  ALP_MAD_Joey
+//
+//  Created by Kevin Christian on 04/06/25.
+//
+
+import Foundation
---
+++ ALP_MAD_Joey/View/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  ALP_MAD_Joey
+//
+//  Created by Kevin Christian on 05/06/25.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
