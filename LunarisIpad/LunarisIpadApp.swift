5eb458760bb4af134677d8bc442acb2a3596b8a2
Calvin05003 <calvinlaiman@gmail.com>
2025-06-11 00:13:39 +0700

    ipad otw

---
+++ LunarisIpad/LunarisIpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarisIpadApp.swift
+//  LunarisIpad
+//
+//  Created by Calvin Laiman on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LunarisIpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LunarisWat Watch App/LunarisWatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunarisWatApp.swift
+//  LunarisWat Watch App
+//
+//  Created by Calvin Laiman on 10/06/25.
+//
+
+import SwiftUI
+
+@main
+struct LunarisWat_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
